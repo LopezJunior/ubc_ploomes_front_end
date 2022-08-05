a0b7c49ff6c285373c1e355bca5cbd5a538a0b7f
Maurício Júnior <456junior456@gmail.com>
2022-08-04 23:05:56 -0300

    Teste, imagem n atela

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import './App.css';
-import { ThemeProvider } from 'styled-components';
-import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import "./App.css";
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Teste from "pages";
 
 const queryClient = new QueryClient();
 
 function App() {
-	return (
-		<>
-			<BrowserRouter>
-				<QueryClientProvider client={queryClient}>
-					<></>
-				</QueryClientProvider>
-			</BrowserRouter>
-		</>
-	);
+  return (
+    <>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <Teste />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+const Teste = () => {
+  return (
+    <div>
+      <img src="https://i.imgur.com/nV66Ng2.png" className="image" />
+    </div>
+  );
+};
+
+export default Teste;
