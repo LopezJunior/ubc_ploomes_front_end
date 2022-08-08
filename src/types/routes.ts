9df1065b818879e37c7b20297131949d35eb2e6e
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-08-08 20:19:16 -0300

    acrescentando rotas

--- src/types/routes.ts
+++ src/types/routes.ts
@@ -2,4 +2,11 @@ export enum RoutePath {
 	ROOT = '/',
 	AUTHLOGIN = '/authlogin',
 	HOMEPAGE = '/homepage',
+	CREATEUSER = '/createuser',
+	USERFORGOTPASSWORD = '/userforgotpassword',
+	MYACCOUNT = '/myaccount',
+	LOGOUT = '/logout',
+	FILTERBYMONEY = '/filterbymoney',
+	MYBINGO = '/mybingo',
+	RESETSECTION = '/resetsection',
 }
