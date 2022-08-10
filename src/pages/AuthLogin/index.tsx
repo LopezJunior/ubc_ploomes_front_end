ed4ab8eaa22180c0f0a8fda3af78c8f9ef039968
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-08-09 23:12:58 -0300

    correção chamada img

--- src/pages/AuthLogin/index.tsx
+++ src/pages/AuthLogin/index.tsx
@@ -1,12 +1,12 @@
 import BoxLogin from 'components/BoxLogin';
 import * as S from './style';
-import Logo from '../../assets/img/logo_cinturao.png';
+import logo from '../../assets/img/logo_cinturao.png';
 
 const AuthLogin = () => {
 	return (
 		<S.AuthLogin>
 			<S.LoginContent>
-				<img src={Logo} alt="" />
+				<img src={logo} alt="" />
 				<BoxLogin />
 			</S.LoginContent>
 		</S.AuthLogin>
