b1640f1d66b05202e569864f80e4b4d89facbe70
Jefferson Silva <developer.jefferson27@outlook.com>
2022-08-11 23:03:30 -0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -4,8 +4,6 @@ import ConfigStyled from 'pages/ConfigStyled';
 import AuthLogin from "pages/AuthLogin";
 import CreateUser from "pages/CreateUser";
 import HomePage from "pages/Homepage";
-import { Route, Routes } from "react-router-dom";
-import { RoutePath } from "types/routes";
 
 const Router = () => {
   return (
@@ -13,5 +11,9 @@ const Router = () => {
       <Route path={RoutePath.ROOT} element={<ConfigStyled />} />
       <Route path={RoutePath.HOMEPAGE} element={<HomePage />} />
       <Route path={RoutePath.AUTHLOGIN} element={<AuthLogin />} />
+      <Route path={RoutePath.CREATEUSER} element={<CreateUser />} />
+		</Routes>
+	);
+};
 
 export default Router;
