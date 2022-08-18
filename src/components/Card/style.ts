0505a5f55f97625ef39b52e8b022b506d63a20dd
Maurício Júnior <456junior456@gmail.com>
2022-08-17 22:05:50 -0300

    feature: tabela com Grid

--- src/components/Card/style.ts
+++ src/components/Card/style.ts
@@ -10,39 +10,88 @@ export const Parent = styled.div`
     border-radius: 10px;
     position: relative;
     bottom: -20%;
-  `}
-`;
-
-export const Line = styled.div`
-  ${({ theme }) => css`
-    margin-top: 10px;
-    font-size: 40px;
-    display: flex;
-    justify-content: space-around;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: repeat(5, 1fr);
+    grid-column-gap: 1px;
+    grid-row-gap: 1px;
+    font-size: 30px;
     align-items: center;
-    border-top: 1px solid ${theme.colors.textColorRed};
   `}
 `;
-export const LineBingo = styled.div`
-  margin-top: 10px;
-  font-size: 50px;
-  display: flex;
-  justify-content: space-around;
-  align-items: center; ;
+export const Number01 = styled.div`
+  grid-area: 1 / 1 / 2 / 2;
 `;
-export const Number1 = styled.span`
-  ${({ theme }) => css`
-    /* border-right: 1px solid ${theme.colors.textColorRed}; */
-  `}
+export const Number02 = styled.div`
+  grid-area: 2 / 1 / 3 / 2;
+`;
+export const Number03 = styled.div`
+  grid-area: 3 / 1 / 4 / 2;
+`;
+export const Number04 = styled.div`
+  grid-area: 4 / 1 / 5 / 2;
+`;
+export const Number05 = styled.div`
+  grid-area: 5 / 1 / 6 / 2;
+`;
+export const Number06 = styled.div`
+  grid-area: 1 / 2 / 2 / 3;
+`;
+export const Number07 = styled.div`
+  grid-area: 2 / 2 / 3 / 3;
+`;
+export const Number08 = styled.div`
+  grid-area: 3 / 2 / 4 / 3;
+`;
+export const Number09 = styled.div`
+  grid-area: 4 / 2 / 5 / 3;
 `;
 
-export const Number2 = styled.span``;
-export const Number3 = styled.span``;
-export const Number4 = styled.span``;
-export const Number5 = styled.span``;
-
-export const Bingo1 = styled.span``;
-export const Bingo2 = styled.span``;
-export const Bingo3 = styled.span``;
-export const Bingo4 = styled.span``;
-export const Bingo5 = styled.span``;
+export const Number10 = styled.div`
+  grid-area: 5 / 2 / 6 / 3;
+`;
+export const Number11 = styled.div`
+  grid-area: 1 / 3 / 2 / 4;
+`;
+export const Number12 = styled.div`
+  grid-area: 2 / 3 / 3 / 4;
+`;
+export const Number13 = styled.div`
+  grid-area: 3 / 3 / 4 / 4;
+`;
+export const Number14 = styled.div`
+  grid-area: 4 / 3 / 5 / 4;
+`;
+export const Number15 = styled.div`
+  grid-area: 5 / 3 / 6 / 4;
+`;
+export const Number16 = styled.div`
+  grid-area: 1 / 4 / 2 / 5;
+`;
+export const Number17 = styled.div`
+  grid-area: 2 / 4 / 3 / 5;
+`;
+export const Number18 = styled.div`
+  grid-area: 3 / 4 / 4 / 5;
+`;
+export const Number19 = styled.div`
+  grid-area: 4 / 4 / 5 / 5;
+`;
+export const Number20 = styled.div`
+  grid-area: 5 / 4 / 6 / 5;
+`;
+export const Number21 = styled.div`
+  grid-area: 1 / 5 / 2 / 6;
+`;
+export const Number22 = styled.div`
+  grid-area: 2 / 5 / 3 / 6;
+`;
+export const Number23 = styled.div`
+  grid-area: 3 / 5 / 4 / 6;
+`;
+export const Number24 = styled.div`
+  grid-area: 4 / 5 / 5 / 6;
+`;
+export const Number25 = styled.div`
+  grid-area: 5 / 5 / 6 / 6;
+`;
