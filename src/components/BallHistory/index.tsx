97e171b89c8fb637b71a3fdfc6357f6921af97c5
Maurício Júnior <456junior456@gmail.com>
2022-08-19 20:24:28 -0300

    update url

--- src/components/BallHistory/index.tsx
+++ src/components/BallHistory/index.tsx
@@ -3,8 +3,10 @@ import * as S from "./style";
 
 const BallHistory = () => {
   const numberHistory: number[] = [1, 2, 3, 4, 5, 6];
-  const numberSort: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
-
+  const numberSort: number[] = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25,
+  ];
   const [control, setControl] = useState<number>(0);
   setTimeout(() => {
     setControl(control + 1);
