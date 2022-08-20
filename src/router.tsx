d0fee18cb3634642635152d5cd5fde3fe47bdb0a
Maurício Júnior <456junior456@gmail.com>
2022-08-20 18:53:43 -0300

    update max sorteios

--- src/router.tsx
+++ src/router.tsx
@@ -5,18 +5,20 @@ import AuthLogin from "pages/Login";
 import CreateUser from "pages/Cadastro";
 import HomePage from "pages/Home";
 import MyBingo from "pages/Bingo";
-import VictoryModal from 'components/VictoryModal';
+import VictoryModal from "components/VictoryModal";
+import Teste from "pages/Teste";
 
 const Router = () => {
-	return (
-		<Routes>
-			{/* <Route path={'/teste'} element={<VictoryModal />} /> */}
-			<Route path={RoutePath.HOMEPAGE} element={<HomePage />} />
-			<Route path={RoutePath.AUTHLOGIN} element={<AuthLogin />} />
-			<Route path={RoutePath.CREATEUSER} element={<CreateUser />} />
-			<Route path={RoutePath.MYBINGO} element={<MyBingo />} />
-		</Routes>
-	);
+  return (
+    <Routes>
+      {/* <Route path={'/teste'} element={<VictoryModal />} /> */}
+      <Route path={RoutePath.HOMEPAGE} element={<HomePage />} />
+      <Route path={RoutePath.TESTE} element={<Teste />} />
+      <Route path={RoutePath.AUTHLOGIN} element={<AuthLogin />} />
+      <Route path={RoutePath.CREATEUSER} element={<CreateUser />} />
+      <Route path={RoutePath.MYBINGO} element={<MyBingo />} />
+    </Routes>
+  );
 };
 
 export default Router;
