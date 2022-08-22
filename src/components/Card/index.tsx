949d3c698f5a3885c8b84cc79ff308980688f780
Jefferson Silva <developer.jeffrock27@outlook.com>
2022-08-22 10:35:24 -0300

    fix css styled grid

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,35 +1,35 @@
 import React from "react";
 import * as S from "./style";
-import "./style.css";
+
 
 const Card = () => {
-  let numbersSort: number[] = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
-    22, 23, 24, 25,
-  ];
+	let numbersSort: number[] = [
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+		22, 23, 24, 25,
+	];
 
-  return (
-    <>
-      <S.Parent>
-        <S.Title>
-          <S.TitleB>b</S.TitleB>
-          <S.TitleI>i</S.TitleI>
-          <S.TitleN>n</S.TitleN>
-          <S.TitleG>g</S.TitleG>
-          <S.TitleO>o</S.TitleO>
-        </S.Title>
-        <S.Ball>
-          {numbersSort.map<React.ReactNode>((n: any, index) => {
-            return (
-              <div key={index} className={`Number${n}`}>
-                {n}
-              </div>
-            );
-          })}
-        </S.Ball>
-      </S.Parent>
-    </>
-  );
+	return (
+		<>
+			<S.Parent>
+				<S.Title>
+					<S.TitleB>b</S.TitleB>
+					<S.TitleI>i</S.TitleI>
+					<S.TitleN>n</S.TitleN>
+					<S.TitleG>g</S.TitleG>
+					<S.TitleO>o</S.TitleO>
+				</S.Title>
+				<S.Ball>
+					{numbersSort.map<React.ReactNode>((n: any, index) => {
+						return (
+							<div key={index} className={`Number${n}`}>
+								{n}
+							</div>
+						);
+					})}
+				</S.Ball>
+			</S.Parent>
+		</>
+	);
 };
 
 export default Card;
