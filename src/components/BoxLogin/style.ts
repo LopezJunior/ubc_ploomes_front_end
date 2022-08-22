41983c00e6df7ec6abe855dda0c52e27e95c79ab
Jefferson Silva <developer.jeffrock27@outlook.com>
2022-08-22 10:17:55 -0300

    consertando itens de responsividade

--- src/components/BoxLogin/style.ts
+++ src/components/BoxLogin/style.ts
@@ -1,64 +1,82 @@
 import styled, { css } from "styled-components";
 
 export const BoxLogin = styled.section`
-  ${({ theme }) => css`
-    color: ${theme.colors.textColorRed};
-    font-family: ${theme.constants.bodyFontFamily};
-    background-color: #fff;
-    width: 567px;
-    height: 590px;
-    border-radius: 0.5rem;
-    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
-      rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
-      rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
-  `}
+	${({ theme }) => css`
+		color: ${theme.colors.textColorRed};
+		font-family: ${theme.constants.bodyFontFamily};
+		background-color: #fff;
+		width: 30%;
+		height: 70%;
+		border-radius: 0.5rem;
+		box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+			rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+			rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+
+		@media (max-height: 700px) {
+			width: 35%;
+			height: 75%;
+		}
+	`}
 `;
 
 export const BoxLoginTitle = styled.div`
-  ${() => css`
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    height: 28%;
-    justify-content: center;
-    align-items: center;
-  `}
+	${() => css`
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		height: 28%;
+		justify-content: center;
+		align-items: center;
+	`}
 `;
 
 export const BoxLoginH2 = styled.h2`
-  ${() => css`
-    font-size: 42px;
-    font-weight: 400;
-  `}
+	${() => css`
+		font-size: 2.8rem;
+		font-weight: 400;
+	`}
+
+	@media (max-height: 700px) {
+		font-size: 1.5rem;
+	}
 `;
 
 export const BoxLoginH3 = styled.h3`
-  ${({ theme }) => css`
-    margin-top: -2rem;
-    font-size: 18px;
-    font-weight: 400;
+	${({ theme }) => css`
+		margin-top: -2rem;
+		font-size: 1.1rem;
+		font-weight: 400;
 
-    a {
-      text-decoration: none;
-      color: ${theme.colors.textColorRed};
-    }
+		a {
+			text-decoration: none;
+			color: ${theme.colors.textColorRed};
+		}
 
-    a:hover {
-      color: ${theme.colors.primaryColorOpacity};
-    }
-  `}
+		a:hover {
+			color: ${theme.colors.primaryColorOpacity};
+		}
+
+		@media (max-height: 700px) {
+			margin-top: -1rem;
+			font-size: 1rem;
+		}
+	`}
 `;
 
 export const BoxLoginForm = styled.form`
-  ${() => css`
-    width: 100%;
-    height: 58%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 4rem;
-  `}
+	${() => css`
+		width: 100%;
+		height: 58%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 4rem;
+
+		@media (max-height: 700px) {
+			margin-top: 1.5rem;
+		}
+	`}
 `;
 
 export const BoxLoginSubmit = styled.div`
--- src/components/Card/style.ts
+++ src/components/Card/style.ts
@@ -1,43 +1,49 @@
 import styled, { css } from "styled-components";
 
 export const Parent = styled.div`
-  ${({ theme }) => css`
-    color: ${theme.colors.textColorRed};
-    font-family: ${theme.constants.bodyFontFamily};
-    background-color: white;
-    width: 22%;
-    height: 310px;
-    border-radius: 10px;
-    position: relative;
-    bottom: -20%;
-    margin-left: 20px;
-  `}
+	${({ theme }) => css`
+		color: ${theme.colors.textColorRed};
+		font-family: ${theme.constants.bodyFontFamily};
+		background-color: white;
+		width: 22%;
+		height: 48%;
+		border-radius: 10px;
+		position: relative;
+		bottom: -17%;
+		margin-left: 20px;
+	`}
 `;
 
 export const Ball = styled.div`
-  margin-left: 20px;
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  grid-template-rows: repeat(5, 1fr);
-  grid-column-gap: 1px;
-  grid-row-gap: 1px;
-  font-size: 45px;
-  align-items: center;
+	margin-left: -1px;
+	display: grid;
+	grid-template-columns: repeat(5, 2fr);
+	grid-template-rows: repeat(5, 2fr);
+	grid-column-gap: 1px;
+	grid-row-gap: 1px;
+	font-size: 45px;
+	text-align: center;
+
+	@media (min-height: 700px) {
+		grid-column-gap: 1px;
+		grid-row-gap: 20px;
+		font-size: 55px;
+	}
 `;
 
 export const Title = styled.div`
-  ${({ theme }) => css`
-    margin: 5px;
-    text-align: center;
-    font-size: 60px;
-    display: grid;
-    grid-template-columns: repeat(5, 1fr);
-    grid-template-rows: repeat(5, 1fr);
-    grid-column-gap: 0px;
-    grid-row-gap: 0px;
-    height: 50px;
-    border-bottom: 1px solid ${theme.colors.textColorRed};
-  `}
+	${({ theme }) => css`
+		margin: 5px;
+		text-align: center;
+		font-size: 60px;
+		display: grid;
+		grid-template-columns: repeat(5, 1fr);
+		grid-template-rows: repeat(5, 1fr);
+		grid-column-gap: 0px;
+		grid-row-gap: 0px;
+		height: 17%;
+		border-bottom: 1px solid ${theme.colors.textColorRed};
+	`}
 `;
 
 export const TitleB = styled.div`
