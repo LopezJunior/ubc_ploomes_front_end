8af8a7b31718a11fd8f938017fe22fdf80dad78e
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-08-22 22:26:19 -0300

    criando função de seleção dos numeros da cartela

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,12 +1,14 @@
-import React from "react";
-import * as S from "./style";
-
+import React, { useState } from 'react';
+import * as S from './style';
 
 const Card = () => {
 	let numbersSort: number[] = [
 		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
 		22, 23, 24, 25,
 	];
+
+	const [isSelect, setIsSelect] = useState(() => numbersSort.map(() => false));
+	const [numberList, setNumberList] = useState<any[]>([]);
 
 	return (
 		<>
@@ -21,8 +23,31 @@ const Card = () => {
 				<S.Ball>
 					{numbersSort.map<React.ReactNode>((n: any, index) => {
 						return (
-							<div key={index} className={`Number${n}`}>
-								{n}
+							<div
+								key={index}
+								className={`Number${n}`}
+								onClick={() => {
+									setIsSelect(
+										isSelect.map((bool, j) => {
+											if (j === index) {
+												const isDuplicate = numberList.findIndex(
+													(num) => num === n
+												);
+												if (isDuplicate === -1) {
+													setNumberList((numberList) => [...numberList, n]);
+												}
+												return true;
+											} else {
+												return bool;
+											}
+										})
+									);
+								}}
+							>
+								<div className={`${isSelect[index] ? 'selected' : ''}`}>
+									{n}
+									{console.log(numberList)}
+								</div>
 							</div>
 						);
 					})}
