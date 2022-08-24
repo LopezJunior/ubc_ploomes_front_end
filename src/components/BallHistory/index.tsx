ec45d3f5cf6a84dc9421838c1842059326945908
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-08-23 23:22:11 -0300

    jeff/history-test

--- src/components/BallHistory/index.tsx
+++ src/components/BallHistory/index.tsx
@@ -1,14 +1,15 @@
-import React, { useState } from "react";
-import * as S from "./style";
+import React, { useEffect, useState } from 'react';
+import * as S from './style';
 
 const BallHistory = () => {
-	const maxSort = 10;
-	const numberHistory: number[] = [1, 2, 3, 4, 5, 6];
+	const maxSort = 30;
+	/* const numberHistory2: number[] = [1, 2, 3, 4, 5, 6]; */
+	const [numberHistory, setNumberHistory] = useState<number[]>([]);
 
 	const numberBackSort: number[] = [
 		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 	];
-	const tempBackEnd = 1;
+	const tempBackEnd = 2;
 	const [time, setTime] = useState(tempBackEnd);
 	const [control, setControl] = useState<number>(0);
 
@@ -17,7 +18,14 @@ const BallHistory = () => {
 
 	setTimeout(() => {
 		setControl(control + 1);
+		console.log(control);
 	}, time * 1000);
+
+	useEffect(() => {
+		setNumberHistory(() => [...numberHistory, control]);
+	}, [control]);
+
+	console.log(numberHistory, control);
 
 	return (
 		<S.HistoryContainer>
