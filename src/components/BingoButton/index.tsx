638da7636aa93f9045995702d81bead2664677ff
Maurício Júnior <456junior456@gmail.com>
2022-08-25 18:02:49 -0300

    update rotas

--- src/components/BingoButton/index.tsx
+++ src/components/BingoButton/index.tsx
@@ -1,30 +1,46 @@
 import React, { useEffect, useState, useContext } from "react";
-import { RoomContext  } from "Contexts/room";
+import { RoomContext } from "Contexts/room";
+import Api from "../../services/api";
+import swal from "sweetalert";
 
 const BingoButton = () => {
-    const context = useContext( RoomContext );
+  const context = useContext(RoomContext);
 
-    const handleClick = () => {
-        const card01 ={
-            id: '01',
-            'numeros': context?.getNumbers(),
-            'marcacao': context?.selecteds,
-        }
-        console.log('card 01:', card01 );
+  const handleClick = async () => {
+    const card01 = {
+      id: "01",
+      marcacao: context?.selecteds,
+      numeros: context?.getNumbers(),
+    };
+    console.log("card 01:", RoomContext);
 
+    try {
+      const res = await Api.post(`/Room/room/checkBingo`, card01);
+
+      return res.data;
+    } catch (error: any) {
+      swal({
+        title: "Error",
+        text: `${error.message}`,
+        icon: "error",
+        timer: 6000,
+      });
+      return error;
     }
-
-    useEffect( () => {
-    }, []);
-
-    return (
-      <>
-        <div style={{display:"flex",justifyContent:"flex-end"}}>
-            <button onClick={handleClick}>Bingo</button>
-        </div>
-      </>
-    );
   };
-  
-  export default BingoButton;
-  
+
+  // return res.data;
+
+  // useEffect( () => {
+  // }, []);
+
+  return (
+    <>
+      <div style={{ display: "flex", justifyContent: "flex-end" }}>
+        <button onClick={handleClick}>Bingo</button>
+      </div>
+    </>
+  );
+};
+
+export default BingoButton;
