fb9d0d05111b05cd28e954aaebae2c58fd8b3f43
Jefferson Silva <developer.jeffrock27@outlook.com>
2022-08-29 16:32:48 -0300

    quase funcionando

--- src/components/BallHistory/index.tsx
+++ src/components/BallHistory/index.tsx
@@ -5,50 +5,52 @@ import { Room, RoomConfig } from 'components/StartButton/type';
 
 const BallHistory = () => {
 	const status = useLocation();
-	const maxSort = 30;
-	let [lastNumberHistory, setLastNumberHistory] = useState<number[]>([
-		0, 0, 0, 0, 0, 0,
-	]);
-	const [numberHistory, setNumberHistory] = useState<number[]>([]);
-	let [backSort, setBackSort] = useState<any[]>([]);
-	const tempBackEnd = 30;
-	const [time, setTime] = useState(tempBackEnd);
+	const [maxSort, setMaxSort] = useState(1);
+	let [lastNumberHistory, setLastNumberHistory] = useState<any[]>([0]);
+	const [numberHistory, setNumberHistory] = useState<any[]>([]);
+	const [time, setTime] = useState(10);
 	const [control, setControl] = useState<number>(0);
-
 	const [room, setRoom] = useState<Room>();
 
 	async function getState() {
 		let roomParams = status.state as RoomConfig;
-		backSort[control] = roomParams.room.prizeOrder.map((e) => e);
+
 		setTime(+roomParams.room.frequency);
 		setRoom(roomParams.room);
-		console.log(
-			'teste',
-			backSort.map((e) => e)
-		);
+		setMaxSort(Number(roomParams.room.limitPrizeDraw))
 	}
 
-	useEffect(() => {
-		setNumberHistory(() => [...numberHistory, result[control]]);
-		for (let i = 0; i < 6; i++) {
-			lastNumberHistory[i] = numberHistory[numberHistory.length - (i + 1)];
-		}
-		getState();
-	}, [control]);
-
-	let result = backSort.splice(0, maxSort);
-	/*  console.log(result); */
-
 	setTimeout(() => {
-		if (control < result.length) {
+		if (control < Number(room?.prizeOrder.length)) {
 			setControl(control + 1);
 		}
 	}, time * 1000);
+	
+	useEffect(() => {
+		getState();
+		setNumberHistory((numberHistory: any ) => [...numberHistory, room?.prizeOrder[control-1]])
+
+		console.log(lastNumberHistory.length)
+
+		if(lastNumberHistory.length && lastNumberHistory[0] === undefined){
+			lastNumberHistory = lastNumberHistory.filter(e => {
+				return e !== undefined;
+			})
+			
+			console.log('Aqui ', lastNumberHistory )
+		}
+		for (let i = 0; i < 6; i++) {
+			lastNumberHistory[i] = numberHistory[numberHistory.length - (i+1)];
+		}
+		
+	}, [control]);
+
+	//let result = backSort.splice(0, maxSort);
 
 	return (
 		<S.HistoryContainer>
 			<>
-				<S.Ball>{result[control]}</S.Ball>
+				<S.Ball>{room?.prizeOrder[control]}</S.Ball>
 				{lastNumberHistory.map<React.ReactNode>((n, index) => {
 					return <S.BallDraw key={index}>{n}</S.BallDraw>;
 				})}
