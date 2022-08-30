818a12f5ebca23879d0998492f19cae735cf5859
Jefferson Silva <developer.jeffrock27@outlook.com>
2022-08-30 10:48:37 -0300

    historico de bolas e cronometro

--- src/components/FooterBingo/index.tsx
+++ src/components/FooterBingo/index.tsx
@@ -1,11 +1,36 @@
 import ButtonRedLarge from "components/ButtonRedLarge";
 import * as S from "./style";
+import { useLocation } from "react-router-dom";
+import { Room, RoomConfig } from "components/StartButton/type";
+import { useEffect, useState } from "react";
 
 const FooterBingo = () => {
+  const status = useLocation();
+  const [time, setTime] = useState(2);
+  const [control, setControl] = useState<number>(0);
+
+  async function getState() {
+    let roomParams = status.state as RoomConfig;
+    setTime(+roomParams.room.frequency);
+  }
+
+  useEffect(() => {
+		
+    getState();
+  }, []);
+
+  setTimeout(() => {
+    setControl(time);
+    if(control > 1){
+      let temp = control - 1;
+      setControl(temp)
+    }
+  }, 1000);
+
   return (
     <S.TitleBingo>
       <S.StopWatch>
-        <S.Number>10</S.Number>
+        <S.Number>{control}</S.Number>
         <S.Seconds>Segundos</S.Seconds>
       </S.StopWatch>
       <S.Title>UBC</S.Title>
