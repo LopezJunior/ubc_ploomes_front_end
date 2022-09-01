51dd79d10d2293ac69fa4437604735e25c52bf57
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-08-31 22:08:25 -0300

    fix select number

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,70 +2,86 @@ import React, { useState, useEffect, useContext } from "react";
 import * as S from "./style";
 import { RoomContext } from "Contexts/room";
 import { Vetor } from "components/StartButton/type";
+import classNames from 'classnames';
 
 const Card = () => {
-  /* const [cards, setCards] = useState<Vetor[]>(); */
-  const context = useContext(RoomContext);
-  const cardNumbers = Number(context?.getCards()!.length);
-  let isSelect: any = [[]];
+	/* const [cards, setCards] = useState<Vetor[]>(); */
+	const context = useContext(RoomContext);
+	const cardNumbers = Number(context?.getCards()!.length);
+	const [isSelect, setIsSelect] = useState<any[][]>(
+		new Array(cardNumbers).fill(false).map(() => new Array(25).fill(false))
+	);
 
-  //const [numberList, setNumberList] = useState<any[]>([]);
-  
-  console.log(isSelect)
-  
-  /*
+	//const [numberList, setNumberList] = useState<any[]>([]);
+
+	/*
   async function initialize() {
     setCards(context?.getCards()!);
   }
 */
-  useEffect(() => {
-    for(let i = 0; i < cardNumbers; i++){
-      isSelect[i] = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,]
-    }
+	useEffect(() => {
+		/*  initialize(); */
+	}, []);
 
-   /*  initialize(); */
-  }, []);
-  
-
-  return (
-    <>
-      <S.Content>
-        {context?.getCards()!.map<React.ReactNode>((card, cardIndex) => (
-          <S.Parent key={cardIndex}>
-            <S.Title>
-              <S.TitleB>b</S.TitleB>
-              <S.TitleI>i</S.TitleI>
-              <S.TitleN>n</S.TitleN>
-              <S.TitleG>g</S.TitleG>
-              <S.TitleO>o</S.TitleO>
-            </S.Title>
-            <S.Ball>
-              {card.vetor.map<React.ReactNode>((n: any, index) => {
-                return (
-                  <div
-                    key={index}
-                    className={`Number${index + 1}`}
-                    onClick={() => {
-                      
-                        isSelect[cardIndex][index] = true
-                      
-                          console.log( isSelect[cardIndex][index])
-                    }}
-                  >
-                    <div className={`${()=>isSelect[cardIndex][index] ? "selected" : ""}`}>
-                      {n}
-                      {/*console.log(numberList)*/}
-                    </div>
-                  </div>
-                );
-              })}
-            </S.Ball>
-          </S.Parent>
-        ))}
-      </S.Content>
-    </>
-  );
-};
+	return (
+		<>
+			<S.Content>
+				{context?.getCards()!.map<React.ReactNode>((card, cardIndex) => (
+					<S.Parent key={cardIndex}>
+						<S.Title>
+							<S.TitleB>b</S.TitleB>
+							<S.TitleI>i</S.TitleI>
+							<S.TitleN>n</S.TitleN>
+							<S.TitleG>g</S.TitleG>
+							<S.TitleO>o</S.TitleO>
+						</S.Title>
+						<S.Ball>
+							{card.vetor.map<React.ReactNode>((n: any, index) => {
+								return (
+									<div
+										key={index}
+										className={`Number${index + 1}`}
+										onClick={() => {
+											if (!isSelect[cardIndex][index]) {
+												let copy = [...isSelect];
+												copy[cardIndex][index] = true;
+												setIsSelect(copy);
+											} else {
+												let copy = [...isSelect];
+												copy[cardIndex][index] = false;
+												setIsSelect(copy);
+											}
+											if (!card.selecteds) {
+												card.selecteds = [];
+											}
+											const isDuplicate = card.selecteds.findIndex(
+												(num) => num === n
+											);
+											if (isDuplicate === -1) {
+												card.selecteds.push(n);
+											} else {
+												card.selecteds.filter((item) => item !== n);
+											}
+										}}
+									>
+										<div
+											className={`${
+												isSelect[cardIndex][index] ? 'selected' : ''
+											}`}
+										>
+											{n}
+											{/*console.log(numberList)*/}
+										</div>
+									</div>
+								);
+							})}
+						</S.Ball>
+					</S.Parent>
+				))}
+			</S.Content>
+		</>
+	);
+};;;;;;;
 
 export default Card;
 
